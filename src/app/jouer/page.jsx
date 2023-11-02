0b79ae542c3ebd08f8d1c79e553843a7978a0be6
gumimacihun <gumimacihun@gmail.com>
2023-11-02 19:43:37 +0100

    Changes to be committed: 	new file:   public/FiveM-Symbole.jpg 	modified:   src/app/jouer/page.jsx 	modified:   src/app/support/page.jsx

--- src/app/jouer/page.jsx
+++ src/app/jouer/page.jsx
@@ -1,3 +1,33 @@
+import Image from "next/image";
+
+import FivemBg from "@/../public/FiveM-Symbole.jpg";
+import Link from "next/link";
+
 export default function Jouer() {
-  return <div>Jouer</div>;
+  return (
+    <section className="">
+      <div className="relative overflow-hidden rounded-lg">
+        <Image
+          src={FivemBg}
+          width={1920}
+          height={1080}
+          className="w-full h-full scale-105 blur-xl"
+        />
+        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+          <Link
+            href="fivem://connect/194.62.1.15:30120"
+            className="flex items-center gap-2 px-5 py-2 text-lg font-bold text-white uppercase transition duration-300 ease-in-out bg-orange-500 rounded-lg shadow-2xl fill-white hover:scale-105 hover:bg-white hover:text-orange-600 hover:fill-orange-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 384 512"
+              className="text-2xl">
+              <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
+            </svg>
+            Jouer sur FiveStars !
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/app/support/page.jsx
+++ src/app/support/page.jsx
@@ -29,7 +29,7 @@ export default function Support() {
             className="text-6xl fill-orange-600">
             <path d="M0 48C0 21.5 21.5 0 48 0l0 48V441.4l130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4V48H48V0H336c26.5 0 48 21.5 48 48V488c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488V48z" />
           </svg>
-          <h3 className="text-3xl font-bold text-orange-600">Reglement</h3>
+          <h3 className="text-3xl font-bold text-orange-600">Règlement</h3>
           <p className="p-2 font-semibold text-center text-orange-500">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             Repudiandae eligendi, repellat quas tenetur, vero aliquid provident
