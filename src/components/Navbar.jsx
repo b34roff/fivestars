f9a1218f1016906e48f2d07ebefb5c73896bb70d
gumimacihun <gumimacihun@gmail.com>
2023-11-03 11:19:21 +0100

    Changes to be committed: 	modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -81,7 +81,7 @@ export default function Navbar() {
             xmlns="http://www.w3.org/2000/svg"
             height="1em"
             viewBox="0 0 448 512"
-            className="text-2xl fill-orange-600">
+            className="p-1 px-2 m-1 text-3xl bg-orange-600 rounded-lg shadow-lg fill-white">
             <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
           </svg>
         </div>
